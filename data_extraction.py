f0115aee5dfbf48d9a42b35ec7494c81ef1b1c9f
Kevin <your_uni@columbia/barnard.edu>
2024-04-11 14:48:18 -0400

    Added data manipulation files

---
+++ data_extraction.py
@@ -0,0 +1,5 @@
+import BeautifulSoup
+
+def scrape_data():
+    # Placeholder for web scraping implementation
+    return None
---
+++ data_preprocessing.py
@@ -0,0 +1,3 @@
+def preprocess_data(data):
+    # Placeholder for data cleaning and preprocessing implementation
+    return None
