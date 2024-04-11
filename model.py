3c4004ca5a36636a90bbc05bb2079896ee1b8945
Kevin <your_uni@columbia/barnard.edu>
2024-04-11 14:50:09 -0400

    Added ML model

---
+++ model.py
@@ -0,0 +1,11 @@
+class PlayerModel:
+    def __init__(self):
+        pass
+
+    def train(self, data):
+        # Placeholder for training machine learning model
+        pass
+
+    def evaluate(self):
+        # Placeholder for evaluating model performance
+        pass
